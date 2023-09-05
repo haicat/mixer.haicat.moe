ee73286f48483c9fc4b16f4d963b2f45b913acbe
haicat <haicat@users.noreply.github.com>
2023-09-05 03:14:24 -0500

    module

---
+++ serverModule.ts
@@ -0,0 +1,34 @@
+
+const mime = require("mime");
+import * as fs from "fs";
+
+import { IncomingMessage, ServerResponse } from "http";
+
+export default class {
+    name = "mixer";
+
+    match(request : IncomingMessage, extra : any){
+        return extra.subdomain == "mixer";
+    };
+
+    run(request : IncomingMessage, response : ServerResponse, extra : any){
+        let fname = request.url.replace("..","").replace("\\","");
+        if(fname == "/" || fname == ""){
+            fname = "/mixer.index.html";
+        }
+        var path = __dirname + "../static" + fname;
+        fs.stat(path, function(err, stat){
+            if((err==null) && !(fs.lstatSync(path).isDirectory())){
+                response.writeHead(200, {"Content-Type": mime.getType(path)});
+                var fstream = fs.createReadStream(path);
+                fstream.pipe(response);
+                return;
+            } else {
+                response.writeHead(404, {"Content-Type": "text/plain"});
+                response.write("404 - Not Found");
+                response.end();
+            }
+        });
+        return;
+    };
+};
