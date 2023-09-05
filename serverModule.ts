a660c6113faf3ffcf303a3def29ecc6e4b5bc4a3
haicat <haicat@users.noreply.github.com>
2023-09-05 03:54:03 -0500

    aerhgwsrthsrh

--- serverModule.ts
+++ serverModule.ts
@@ -3,6 +3,7 @@ const mime = require("mime");
 import * as fs from "fs";
 
 import { IncomingMessage, ServerResponse } from "http";
+import { UrlWithParsedQuery } from "url";
 
 export default class {
     name = "mixer";
@@ -12,11 +13,13 @@ export default class {
     };
 
     run(request : IncomingMessage, response : ServerResponse, extra : any){
-        let fname = request.url.replace("..","").replace("\\","");
+        
+        //let fname = request.url.replace("..","").replace("\\","");
+        let fname = (extra.url as UrlWithParsedQuery).pathname;
         if(fname == "/" || fname == ""){
             fname = "/mixer.index.html";
         }
-        var path = __dirname + "../static" + fname;
+        var path = __dirname + "/../static" + fname;
         fs.stat(path, function(err, stat){
             if((err==null) && !(fs.lstatSync(path).isDirectory())){
                 response.writeHead(200, {"Content-Type": mime.getType(path)});
